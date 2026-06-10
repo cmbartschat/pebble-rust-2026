56bf23437b85b90d964812c79811fad25786d91f
Christoph Bartschat <christoph@barts.chat>
2026-06-09 18:13:18 -0700

    working

---
+++ build.rs
@@ -0,0 +1,63 @@
+use std::env;
+use std::path::PathBuf;
+
+fn main() {
+    // Tell cargo to look for shared libraries in the specified directory
+
+    // let sdk_root = std::env::var("PEBBLE_SDK_PATH").unwrap_or_else(|_| {
+    //     format!(
+    //         "{}/Library/Application Support/Pebble SDK/SDKs/4.9.169/sdk-core/pebble/basalt",
+    //         std::env::var("HOME").unwrap()
+    //     )
+    // });
+
+    let sdk_root = std::env::var("PEBBLE_SDK_PATH").unwrap_or_else(|_| {
+        format!(
+            "{}/Library/Application Support/Pebble SDK/SDKs/4.9.169/sdk-core/pebble/basalt",
+            std::env::var("HOME").unwrap()
+        )
+    });
+
+    println!("cargo:rustc-link-search={}/lib", sdk_root);
+    println!("cargo:rustc-link-lib=pebble");
+
+    // let toolchain1 = "/Users/cmb/Library/Application Support/Pebble SDK/SDKs/4.9.169/toolchain/arm-none-eabi/arm-none-eabi/include/ssp/";
+    let toolchain2 = "/Users/cmb/Library/Application Support/Pebble SDK/SDKs/4.9.169/toolchain/arm-none-eabi/arm-none-eabi/include";
+
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .clang_arg(format!("-I{}/include", sdk_root))
+        // .clang_arg(format!("-I{}", toolchain1))
+        .clang_arg(format!("-I{}", toolchain2))
+        .clang_arg(
+            "--sysroot=/Users/cmb/Library/Application Support/Pebble SDK/SDKs/4.9.169/toolchain/arm-none-eabi/arm-none-eabi"
+        )
+        // .clang_arg("-D__stdio_h")
+        .clang_arg("-D_TIME_H_")
+        //  .blocklist_type("tm")  
+        .clang_arg("-I/Users/cmb/repo/pebble-rust-2026")
+        .clang_arg("--target=thumbv7m-none-eabi")
+        .use_core()
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
+        .generate()
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut generated_bindings: Vec<u8> = r#"
+#[allow(unnecessary_transmutes)]
+#[allow(unused)]
+#[allow(unsafe_op_in_unsafe_fn)]
+"#
+    .bytes()
+    .collect();
+
+    generated_bindings.reserve(1_000_000);
+
+    bindings
+        .write(Box::new(&mut generated_bindings))
+        .expect("Failed to write");
+
+    std::fs::write(out_path.join("bindings.rs"), generated_bindings)
+        .expect("Couldn't write bindings!");
+}
---
+++ wrapper.h
@@ -0,0 +1,3 @@
+typedef unsigned long time_t;  // match arm-none-eabi's definition
+
+#include <pebble.h>
