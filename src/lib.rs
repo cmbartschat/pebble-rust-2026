77c256b30fe36175c29262209876c315897e4408
Christoph Bartschat <christoph@barts.chat>
2026-06-10 01:57:40 -0700

    export for linking

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+#![no_main]
 #![no_std]
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
@@ -6,19 +7,26 @@
 #![allow(unnecessary_transmutes)]
 #![allow(unsafe_op_in_unsafe_fn)]
 
+use core::{panic::PanicInfo, ptr::null};
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+#[unsafe(no_mangle)]
+pub fn main() -> isize {
+    unsafe {
+        printf(null());
     }
+    0
+}
+
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    // logs "panicked at '$reason', src/main.rs:27:4" to the host stderr
+    // writeln!(host_stderr, "{}", info).ok();
+
+    loop {}
 }
