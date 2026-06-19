1f5ed06f04bcace18c993a50ff32a7dc2f6b7b79
Christoph Bartschat <christoph@barts.chat>
2026-06-19 12:41:44 -0700

    as lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,11 @@
-#![no_main]
 #![no_std]
 
 extern crate alloc;
 
 mod app;
-mod app_cores;
 mod app_message_result;
 mod bitmap;
+mod bitmap_layer;
 pub mod color;
 mod context;
 mod custom_alloc;
@@ -17,18 +16,15 @@ mod key;
 mod layer;
 mod log;
 mod persist;
-mod time;
-
-mod bitmap_layer;
 mod rect;
 pub mod sys;
 mod text_layer;
+mod time;
 mod timer;
 mod window;
 
 pub use crate::app::APP;
 pub use crate::app::InboxSize;
-use crate::app_cores::run_cores;
 pub use crate::app_message_result::AppMessageError;
 pub use crate::bitmap::Bitmap;
 pub use crate::bitmap_layer::BitmapLayer;
@@ -44,9 +40,3 @@ pub use crate::time::Time;
 pub use crate::timer::Timer;
 pub use crate::window::Window;
 pub use proc::*;
-
-#[unsafe(no_mangle)]
-pub fn main() -> i32 {
-    run_cores();
-    0
-}
