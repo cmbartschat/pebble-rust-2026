c9060fb37b96a0025a4add3d613a1869f3668004
Christoph Bartschat <christoph@barts.chat>
2026-06-19 15:44:40 -0700

    simplify memory

--- src/custom_alloc.rs
+++ src/custom_alloc.rs
@@ -1,5 +1,4 @@
 use core::alloc::{GlobalAlloc, Layout};
-use core::ptr;
 
 use crate::log::log_c_str;
 
@@ -12,20 +11,19 @@ const NATIVE_ALIGN: usize = usize::BITS as usize / 8;
 
 unsafe extern "C" {
     pub unsafe fn malloc(size: usize) -> *mut u8;
-    pub unsafe fn aligned_alloc(alignment: usize, size: usize) -> *mut u8;
     pub unsafe fn realloc(ptr: *mut u8, size: usize) -> *mut u8;
     pub unsafe fn free(ptr: *mut u8);
 }
 
 unsafe impl GlobalAlloc for Allocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if layout.align() <= NATIVE_ALIGN {
-            return unsafe { malloc(layout.size()) };
+        if layout.align() > NATIVE_ALIGN {
+            log_c_str(
+                c"  ---   ---   ---   alloc called with align > NATIVE_ALIGN (may cause issues)",
+            );
         }
 
-        log_c_str(c"  ---   ---   ---   alloc custom align");
-
-        unsafe { aligned_alloc(layout.align(), layout.size()) }
+        unsafe { malloc(layout.size()) }
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
@@ -33,28 +31,12 @@ unsafe impl GlobalAlloc for Allocator {
     }
 
     unsafe fn realloc(&self, ptr: *mut u8, layout: Layout, new_size: usize) -> *mut u8 {
-        if layout.align() <= NATIVE_ALIGN {
-            return unsafe { realloc(ptr, new_size) };
+        if layout.align() > NATIVE_ALIGN {
+            log_c_str(
+                c"  ---   ---   ---   realloc called with align > NATIVE_ALIGN (may cause issues)",
+            );
         }
 
-        log_c_str(c"  ---   ---   ---   realloc custom align");
-
-        let new_layout = match Layout::from_size_align(new_size, layout.align()) {
-            Ok(l) => l,
-            Err(_) => return ptr::null_mut(),
-        };
-
-        let new_ptr = unsafe { self.alloc(new_layout) };
-        if new_ptr.is_null() {
-            return ptr::null_mut();
-        }
-
-        let copy_size = core::cmp::min(layout.size(), new_size);
-        unsafe {
-            ptr::copy_nonoverlapping(ptr, new_ptr, copy_size);
-            free(ptr);
-        }
-
-        new_ptr
+        unsafe { realloc(ptr, new_size) }
     }
 }
