cdc8218419b02c55e3824db1e0ccf4043a4e47d7
Christoph Bartschat <christoph@barts.chat>
2026-06-28 17:27:36 -0700

    add status bar layer

---
+++ src/status_bar_layer.rs
@@ -0,0 +1,104 @@
+use core::{ffi::c_uint, ptr::NonNull};
+
+use crate::{
+    Layer,
+    handle::{Handle, new_handle},
+    layer::{ChildLayer, LayerInner},
+    sys::{self, GColor},
+};
+
+struct StatusBarLayerInner {
+    raw: NonNull<sys::StatusBarLayer>,
+    base_layer: Layer,
+}
+
+#[derive(Clone)]
+pub struct StatusBarLayer {
+    handle: Handle<StatusBarLayerInner>,
+}
+
+impl ChildLayer for StatusBarLayer {
+    fn remove_from_parent(&self) {
+        self.handle.borrow_mut().base_layer.remove_from_parent();
+    }
+
+    fn is_same(&self, other: &Layer) -> bool {
+        self.handle.borrow().base_layer.is_same(other)
+    }
+
+    fn set_parent(&mut self, other: &mut Layer) {
+        self.handle.borrow_mut().base_layer.set_parent(other);
+    }
+}
+
+impl StatusBarLayer {
+    pub fn new() -> Option<Self> {
+        unsafe {
+            let raw = NonNull::new(sys::status_bar_layer_create())?;
+
+            let base = LayerInner::from_ptr(sys::status_bar_layer_get_layer(raw.as_ptr()), false);
+            let Some(base_layer) = base else {
+                sys::status_bar_layer_destroy(raw.as_ptr());
+                return None;
+            };
+
+            Some(Self {
+                handle: new_handle(StatusBarLayerInner {
+                    raw,
+                    base_layer: Layer {
+                        handle: new_handle(base_layer),
+                    },
+                }),
+            })
+        }
+    }
+
+    pub fn get_background_color(&self) -> GColor {
+        unsafe { sys::status_bar_layer_get_background_color(self.handle.borrow().raw.as_ptr()) }
+    }
+
+    pub fn get_foreground_color(&self) -> GColor {
+        unsafe { sys::status_bar_layer_get_background_color(self.handle.borrow().raw.as_ptr()) }
+    }
+
+    pub fn set_background_color(&self) -> GColor {
+        unsafe { sys::status_bar_layer_get_background_color(self.handle.borrow().raw.as_ptr()) }
+    }
+
+    pub fn set_colors(&mut self, foreground: GColor, background: GColor) {
+        unsafe {
+            sys::status_bar_layer_set_colors(
+                self.handle.borrow_mut().raw.as_ptr(),
+                foreground,
+                background,
+            )
+        }
+    }
+
+    pub fn set_separator_mode(&mut self, mode: StatusBarSeparatorMode) {
+        unsafe {
+            sys::status_bar_layer_set_separator_mode(
+                self.handle.borrow_mut().raw.as_ptr(),
+                mode.into(),
+            )
+        }
+    }
+}
+
+pub enum StatusBarSeparatorMode {
+    None,
+    Dotted,
+}
+
+impl From<StatusBarSeparatorMode> for c_uint {
+    fn from(value: StatusBarSeparatorMode) -> c_uint {
+        match value {
+            StatusBarSeparatorMode::None => {
+                sys::StatusBarLayerSeparatorMode_StatusBarLayerSeparatorModeNone
+            }
+            StatusBarSeparatorMode::Dotted => {
+                sys::StatusBarLayerSeparatorMode_StatusBarLayerSeparatorModeDotted
+            }
+        }
+    }
+}
