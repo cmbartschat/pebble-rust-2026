e3c909a29f2271bb5c718d246e7e4b724deaa91c
Christoph Bartschat <christoph@barts.chat>
2026-07-23 20:29:22 -0700

    Save

--- src/layer.rs
+++ src/layer.rs
@@ -3,10 +3,14 @@ use core::ptr::NonNull;
 use alloc::{boxed::Box, rc::Rc, vec::Vec};
 
 use crate::{
-    GRect,
+    GContext, GRect,
     handle::{Handle, WeakHandle, new_handle},
-    sys,
+    log_c_str, sys,
 };
+
+pub struct LayerContext {
+    back_to_self: WeakHandle<LayerInner>,
+}
 
 pub trait ChildLayer {
     fn id(&self) -> usize;
@@ -19,6 +23,7 @@ pub struct LayerInner {
     pub(crate) raw: NonNull<sys::Layer>,
     parent: Option<WeakHandle<LayerInner>>,
     children: Vec<Box<dyn ChildLayer>>,
+    render: Option<Box<dyn Fn(Layer, GContext)>>,
     owned: bool,
 }
 
@@ -37,6 +42,7 @@ impl LayerInner {
             raw: NonNull::new(ptr)?,
             parent: None,
             children: Vec::new(),
+            render: None,
             owned,
         })
     }
@@ -97,11 +103,15 @@ impl ChildLayer for Layer {
 impl Layer {
     pub fn new(frame: GRect) -> Option<Self> {
         unsafe {
-            let layer = sys::layer_create(frame);
+            // Same as sys::layer_create
+            let layer = sys::layer_create_with_data(frame, size_of::<Option<LayerContext>>());
             let handle = LayerInner::from_ptr(layer, true)?;
-            Some(Self {
-                handle: new_handle(handle),
-            })
+            let handle = new_handle(handle);
+            let context = (sys::layer_get_data(layer) as *mut Option<LayerContext>).as_mut()?;
+            *context = Some(LayerContext {
+                back_to_self: Rc::downgrade(&handle),
+            });
+            Some(Self { handle })
         }
     }
 
@@ -131,6 +141,12 @@ impl Layer {
     ) {
         unsafe { sys::layer_set_update_proc(self.as_ptr(), Some(proc)) };
     }
+
+    pub fn set_update(&mut self, callback: Box<dyn Fn(Layer, GContext)>) {
+        self.handle.borrow_mut().render = Some(callback);
+        self.set_update_proc(global_layer_update_handler);
+    }
+
     unsafe fn as_ptr(&self) -> *mut sys::Layer {
         self.handle.borrow_mut().raw.as_ptr()
     }
@@ -150,4 +166,32 @@ impl Layer {
     pub fn remove(&mut self) {
         ChildLayer::remove_from_parent(self);
     }
+}
+
+extern "C" fn global_layer_update_handler(layer: *mut sys::Layer, ctx: *mut sys::GContext) {
+    let ptr = unsafe { (sys::layer_get_data(layer) as *mut LayerContext).as_ref() };
+    let Some(inner_ref) = ptr.as_ref() else {
+        log_c_str(c"Unexpected: Layer data is null");
+        return;
+    };
+    let Some(ctx) = GContext::from_raw(ctx) else {
+        log_c_str(c"Unexpected: Layer context is null");
+        return;
+    };
+    let Some(inner_ref) = inner_ref.back_to_self.upgrade() else {
+        log_c_str(c"Unexpected: Layer inner is destroyed");
+        return;
+    };
+    let layer = inner_ref.borrow();
+    let Some(callback) = &layer.render else {
+        log_c_str(c"Unexpected: Layer has no render function");
+        return;
+    };
+
+    callback(
+        Layer {
+            handle: inner_ref.clone(),
+        },
+        ctx,
+    );
 }
