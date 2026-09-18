83d9c85d04d608c316f69ba5adbccfbac6c8da14
kleines Filmröllchen <kleines@filmroellchen.eu>
2026-09-18 16:43:30 +0200

    Silence various warnings

--- src/sys.rs
+++ src/sys.rs
@@ -1,3 +1,6 @@
+// These are expected and compatible, since they were generated from the system header.
+#![allow(suspicious_runtime_symbol_definitions)]
+
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 pub use bindings::*;
